875828fba99caa784dc6fa944b9290e4f5420ddf
nan2iz <nan2iz.ch@gmail.com>
2017-06-29 21:44:52 -0700

    update projects

--- js/image_popup.js
+++ js/image_popup.js
@@ -13,13 +13,41 @@ $(document).ready(function() {
     "height" : $("#player").css("height")
   });
   
-  $(".button").click(function() {
+  $("#button-bronco").click(function() {
     $(".video-wrapper").fadeIn('fast', function() {
       $(".video").fadeIn(); 
       $(".video").center(); 
     });
         
   });
+    
+  $("#button-qbot").click(function() {
+    $(".video-wrapper-qbot").fadeIn('fast', function() {
+      $(".video").fadeIn(); 
+      $(".video").center(); 
+    });
+        
+  });
+    
+  $("#button-native-form").click(function() {
+    $(".video-wrapper-native").fadeIn('fast', function() {
+      $(".video").fadeIn(); 
+      $(".video").center(); 
+    });
+        
+  });
+    
+  $("#button-healthy-beats").click(function() {
+    $(".video-wrapper-healthy-beats").fadeIn('fast', function() {
+      $(".video").fadeIn(); 
+      $(".video").center(); 
+    });
+        
+  });
+    
+    
+    
+    
   
   $(".video-wrapper").click(function(e) {
     if($(e.target).is(".video-wrapper")) {
@@ -42,5 +70,71 @@ $(document).ready(function() {
     }
     
   });
+    
+ $(".video-wrapper-qbot").click(function(e) {
+    if($(e.target).is(".video-wrapper-qbot")) {
+      $(".video").fadeOut(function() {
+        $(".video-wrapper-qbot").fadeOut(function() {
+         $(".video, .video-wrapper-qbot").css({'display':'none'}); 
+          var src=$("#player").attr("src");
+          $("#player").attr("src", "");
+          $("#player").attr("src", src);
+        });
+      });
+    }
+  });
   
+  $(document).keyup(function(e) {
+    var isShown=$(".video-wrapper-qbot").css("display");
+    
+    if(isShown !== "none" && e.which==27) {
+       $(".video-wrapper-qbot").click(); 
+    }
+    
+  });
+    
+  $(".video-wrapper-native").click(function(e) {
+    if($(e.target).is(".video-wrapper-native")) {
+      $(".video").fadeOut(function() {
+        $(".video-wrapper-native").fadeOut(function() {
+         $(".video, .video-wrapper-native").css({'display':'none'}); 
+          var src=$("#player").attr("src");
+          $("#player").attr("src", "");
+          $("#player").attr("src", src);
+        });
+      });
+    }
+  });
+  
+  $(document).keyup(function(e) {
+    var isShown=$(".video-wrapper-native").css("display");
+    
+    if(isShown !== "none" && e.which==27) {
+       $(".video-wrapper-native").click(); 
+    }
+    
+  });
+  
+  $(".video-wrapper-healthy-beats").click(function(e) {
+    if($(e.target).is(".video-wrapper-healthy-beats")) {
+      $(".video").fadeOut(function() {
+        $(".video-wrapper-healthy-beats").fadeOut(function() {
+         $(".video, .video-wrapper-healthy-beats").css({'display':'none'}); 
+          var src=$("#player").attr("src");
+          $("#player").attr("src", "");
+          $("#player").attr("src", src);
+        });
+      });
+    }
+  });
+  
+  $(document).keyup(function(e) {
+    var isShown=$(".video-wrapper-healthy-beats").css("display");
+    
+    if(isShown !== "none" && e.which==27) {
+       $(".video-wrapper-healthy-beats").click(); 
+    }
+    
+  });
+    
 });
